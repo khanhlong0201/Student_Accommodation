c1594e282e821206c3951a449e8f0d75192eaaa8
Hai Nguyen <nguyenhai15040206@gmail.com>
2023-10-29 12:57:36 +0700

    commit Ui ADMIN

---
+++ BHSystem.Web/Features/Admin/UserPage.razor.cs
@@ -0,0 +1,35 @@
+﻿using BHSytem.Models.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BHSystem.Web.Features.Admin
+{
+    public partial class UserPage
+    {
+        public List<UserModel>? ListUser { get; set; }
+        public IEnumerable<UserModel>? SelectedUsers { get; set; } = new List<UserModel>();
+        public bool IsInitialDataLoadComplete { get; set; } = true;
+        public UserModel UserUpdate { get; set; } = new UserModel();
+
+        public bool IsShowDialog { get; set; }
+        public EditContext? _EditContext { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            ListUser = new List<UserModel>();
+            ListUser.Add(new UserModel() { UserId = 1, FullName = "Nguyễn Tấn Hải" });
+            ListUser.Add(new UserModel() { UserId = 2, FullName = "Nguyễn Tấn Hải" });
+            ListUser.Add(new UserModel() { UserId = 3, FullName = "Nguyễn Tấn Hải" });
+            ListUser.Add(new UserModel() { UserId = 4, FullName = "Nguyễn Tấn Hải" });
+            ListUser.Add(new UserModel() { UserId = 5, FullName = "Nguyễn Tấn Hải" });
+            ListUser.Add(new UserModel() { UserId = 6, FullName = "Nguyễn Tấn Hải" });
+            return base.OnInitializedAsync();
+        }
+
+        protected void OnOpenDialogHandler()
+        {
+            IsShowDialog = true;
+            _EditContext = new EditContext(UserUpdate);
+        }
+
+    }
+}
