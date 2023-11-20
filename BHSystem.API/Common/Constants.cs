a9bce2d77ca3e8c421d15805e586145f96a9a2f6
Hai Nguyen <nguyenhai15040206@gmail.com>
2023-11-20 18:54:11 +0700

    commit SignalR Server/Client

--- BHSystem.API/Common/Constants.cs
+++ BHSystem.API/Common/Constants.cs
@@ -4,5 +4,8 @@
     {
         public const string STORE_USER_UPDATE = "BHM_USER_UPDATE";
         public const string STORE_USER_LOGIN = "BHM_USER_LOGIN";
+
+        public const string HUB_TYPE_BOOKING = "Booking";
+        public const string HUB_TYPE_ROOM = "Room";
     }
 }
--- BHSystem.API/Common/SignalHubProvider.cs
+++ BHSystem.API/Common/SignalHubProvider.cs
@@ -1,18 +1,45 @@
 ﻿using BHSytem.Models.Entities;
+using BHSytem.Models.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BHSystem.API.Common
 {
     public class SignalHubProvider : Hub
     {
-        public void BroadcastEmployee(Users user)
+        /// <summary>
+        /// override lại phương thức connect 
+        /// Tùy biến lưu lại User để gửi thong báo theo User
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
         {
-            _ = Clients.All.SendAsync("ReceiveUsers", user); // gửi cho user
+            await base.OnConnectedAsync();
+            var httpCtx = Context.GetHttpContext();
+            if (httpCtx != null)
+            {
+                var connectionId = Context.ConnectionId;
+                var userIdentifier = httpCtx.Request.Headers["UserName"].ToString();
+                // Lưu thông tin về kết nối của người dùng
+                await Groups.AddToGroupAsync(connectionId, userIdentifier);
+            }
         }
 
-        public void BroadcasrMessage(Messages message)
+        /// <summary>
+        /// disconnect Hub theo User
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            _ = Clients.All.SendAsync("ReceiveMessage", message); // gửi message gì
+            await base.OnDisconnectedAsync(exception);
+            var httpCtx = Context.GetHttpContext();
+            if (httpCtx != null)
+            {
+                var connectionId = Context.ConnectionId;
+                var userIdentifier = httpCtx.Request.Headers["UserName"].ToString();
+                // Lưu thông tin về kết nối của người dùng
+                await Groups.RemoveFromGroupAsync(connectionId, userIdentifier);
+            }
         }
     }
 }
